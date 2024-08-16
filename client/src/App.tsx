7ead0b0e9ce88e6983bc157472209e691ef80a2a
Raveena Liyanage <144007393+raveenaliyanage@users.noreply.github.com>
2024-08-16 23:27:56 +0530

    feat: add product details

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,31 @@ import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  
+
   return (
     <div>
       <header>
         MELLY FASHION
       </header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img src={product.image} 
+              alt={product.name} 
+              className='product-image'
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All Right Reserved</footer>
     </div>
   )
