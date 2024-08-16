d2862b35eea29f584b67642db0c771b2887ef577
Raveena Liyanage <144007393+raveenaliyanage@users.noreply.github.com>
2024-08-16 22:29:42 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  
+  return (
+    <div>
+      <header>
+        MELLY FASHION
+      </header>
+      <main></main>
+      <footer>All Right Reserved</footer>
+    </div>
+  )
+}
+
+export default App
