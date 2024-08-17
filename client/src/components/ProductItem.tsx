e6293ed92e328dc65341d06acfb308fcfc67c2e0
Raveena Liyanage <144007393+raveenaliyanage@users.noreply.github.com>
2024-08-17 20:35:10 +0530

    feat: implement add to cart functionality

--- client/src/components/ProductItem.tsx
+++ client/src/components/ProductItem.tsx
@@ -2,8 +2,28 @@ import { Button, Card } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import { Product } from "../types/Product"
 import Rating from "./Rating"
+import { useContext } from "react"
+import { Store } from "../Store"
+import { CartItem } from "../types/Cart"
+import { convertProductToCartItem } from "../utils"
 
 function ProductItem({ product }: { product: Product }) {
+  const { state, dispatch} = useContext(Store)
+  const {
+    cart: { cartItems },
+  } = state
+  const addToCartHandler = async (item: CartItem) => {
+    const existItem = cartItems.find((x) => x._id === product._id)
+  const quantity = existItem ? existItem.quantity + 1 : 1
+  if (product.countInStock < quantity) {
+    alert('Sorry. Product is out of stock')
+    return
+  }
+  dispatch({
+    type: 'CART_ADD_ITEM',
+    payload: {...item, quantity},
+  })
+  }
     return (
       <Card>
         <Link to={`/product/${product.slug}`}>
@@ -24,7 +44,11 @@ function ProductItem({ product }: { product: Product }) {
               Out of stock
             </Button>
           ) : (
-            <Button>Add to cart</Button>
+            <Button
+            onClick={() => addToCartHandler(convertProductToCartItem(product))}
+          >
+            Add to cart
+          </Button>
           )}
         </Card.Body>
       </Card>
