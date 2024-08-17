8b7634ed623a5aa954662ff9c0cfa80950173159
Raveena Liyanage <144007393+raveenaliyanage@users.noreply.github.com>
2024-08-17 19:21:22 +0530

    feat(server): connect to MongoDB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,24 @@
 import cors from 'cors'
+import dotenv from 'dotenv'
 import express, { Request, Response } from 'express'
-import { sampleProducts } from './data'
+//import { sampleProducts } from './data'
+import mongoose from 'mongoose'
+import { productRouter } from './routers/productRouter'
+import { seedRouter } from './routers/seedRouter'
+
+dotenv.config()
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/MELLYFASHIONdb'
+  mongoose.set('strictQuery', true)
+  mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch(() => {
+    console.log('error mongodb')
+  })
 
 const app = express()
 app.use(
@@ -11,13 +29,8 @@ app.use(
 )
 
 
-app.get('/api/products', (req: Request, res: Response) => {
-  res.json(sampleProducts)
-})
-
-app.get('/api/products/:slug', (req: Request, res: Response) => {
-  res.json(sampleProducts.find((x) => x.slug === req.params.slug))
-})
+app.use('/api/products', productRouter)
+app.use('/api/seed', seedRouter)
 
 const PORT = 4000
 app.listen(PORT, () => {
